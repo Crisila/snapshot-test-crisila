fed07aaa2ca380c6a9f99a929e4c83bcfbbe6735
Crisila <crisilaaban@gmail.com>
2024-01-27 20:57:08 -0800

    add GitHubCard, passed first then edited js to fail test

---
+++ src/GitHubCard.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const GitHubCard = () => {
+     return (
+          <Card>
+               <Card.Img variant="top" src="https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*" alt="Your Photo" />
+               <Card.Body>
+                    <Card.Title>Crisila</Card.Title>
+                    <Card.Text>
+                         Blurb Blurb TEST ONLY
+                         More changes
+                    </Card.Text>
+               </Card.Body>
+          </Card>
+     );
+};
+
+export default GitHubCard;
---
+++ src/GitHubCard.test.js
@@ -0,0 +1,9 @@
+// GitHubCard.test.js
+import React from 'react';
+import renderer from 'react-test-renderer';
+import GitHubCard from './GitHubCard';
+
+test('GitHubCard snapshot', () => {
+     const tree = renderer.create(<GitHubCard />).toJSON();
+     expect(tree).toMatchSnapshot();
+});
