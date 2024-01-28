f7142aedd14f8bd0c74f6a3dfa3c2250b253eb2e
Crisila <crisilaaban@gmail.com>
2024-01-27 20:43:56 -0800

    complete guided coding

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import App from './App';
+// import { renderer } from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders a snapshot', () => {
+//   const tree = renderer.create(<App />).toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
+
+
+import App from './App';
+import { create } from 'react-test-renderer';
+
+test('renders a snapshot', () => {
+  const tree = create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
+
